fd71387cb961b771b2f2ea1d8b990796744e27b4
sarmilaupadhyaya <sarmilaupadhyaya43@gmail.com>
2021-10-12 10:37:25 +0200

    initialization

---
+++ modules/phoeneme_dictionary_extraction.py
@@ -0,0 +1,4 @@
+## this file will be used to extract phonemes from english and french texts and save it to the dictionary
+
+
+import os
---
+++ modules/preprocessing.py
@@ -0,0 +1,15 @@
+import os
+
+
+
+
+class Preprocessing:
+
+    def __init__(self, text, language):
+
+        """
+
+        """
+        pass
+
+
