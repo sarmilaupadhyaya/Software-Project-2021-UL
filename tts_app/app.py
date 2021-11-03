bc8be3c6f00df4dcb13d42b9526b9769446a308a
Rasul <rasul-jasir.dent7@univ-lorraine.fr>
2021-11-03 23:26:25 +0100

    in line audio output

--- tts_app/app.py
+++ tts_app/app.py
@@ -5,22 +5,37 @@ Created on Tue Oct 12 10:30:35 2021
 @author: rasul
 
 """
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, send_file, send_from_directory
+import os
 from os.path import exists
 from gtts import gTTS
 
 app = Flask(__name__)
+#avoid using cached audio
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route("/", methods = ['POST', 'GET'])
 def home():
     if request.method == "POST":
         language = request.form["text_lang"].lower()
-        voice_speaker = request.form["speaker"]
-        string = request.form["text"]        
-        tts =  gTTS(string, lang=language)
-        tts.save('test.mp3')
-        return send_file("test.mp3", as_attachment=True) 
-    else:
+        voice_speaker = request.form["speaker"].lower()
+        string = request.form["text"]
+        #accent 
+        if language == voice_speaker:        
+            tts =  gTTS(string, lang=language, tld = "ca")
+        else:
+            tts = gTTS(string, lang=voice_speaker)
+        tts.save('data/test.mp3')
         return render_template("home.html")
+    else:
+        if exists("test.mp3"):
+            os.remove("test.mp3")
+        return render_template("home.html")
+    
+@app.route("/data/test.mp3", methods = ['GET'])
+def read():
+    return send_from_directory(directory="data", filename="test.mp3", cache_timeout=0)
+
 
 if __name__ == "__main__":
     app.run()
